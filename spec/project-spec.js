ddfdc70899742191e725a5b9d84ec11c9e384875
João Guimarães <jccguimaraes@gmail.com>
2016-05-22 14:06:56 +0100

    chore(travis): travis integration

--- spec/project-spec.js
+++ spec/project-spec.js
@@ -1,7 +1,14 @@
 'use strict';
 
-describe ('when a test is written', function () {
-    it ('should do stuff', function () {
-        expect(true).toEqual(true);
+const project = require('./../src/project');
+
+describe ('project', function () {
+    it ('should have following methods', function () {
+        expect(project.fetchAllViews).toBeDefined();
+        expect(typeof project.fetchAllViews).toEqual('function');
+        expect(project.fetchModelByView).toBeDefined();
+        expect(typeof project.fetchModelByView).toEqual('function');
+        expect(project.fetchAllModels).toBeDefined();
+        expect(typeof project.fetchAllModels).toEqual('function');
     });
 });
