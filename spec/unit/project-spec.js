ec638745940ad20d12de8aa53dec3092ace24b24
João Guimarães <jccguimaraes@gmail.com>
2016-07-16 23:49:51 +0100

    refactor(project): implementing a new look on the project model and view

--- spec/unit/project-spec.js
+++ spec/unit/project-spec.js
@@ -4,44 +4,133 @@ const expect = require('chai').expect;
 
 const project = require('../../src/project');
 
-context ('unit-test :: project', () => {
+context ('unit-test :: project', function () {
 
-    beforeEach (() => {
-        // this sets this.project to a clean state for each test
-        this.project = project;
-    });
+  context ('api', function () {
 
-    afterEach (() => {
-        // this sets this.project to a clean state for each test
-        delete this.project;
-    });
+    context ('#createModel', function () {
 
-    context ('#listAll', () => {
+      it ('is a public method', function () {
+        expect(project.createModel).to.be.a('function');
+      });
 
-        it ('should be a method', () => {
-            expect(this.project.listAll).to.be.a('function');
+      context ('.projectName', function () {
+        it ('should return null if no parameter passed', function () {
+          let model = project.createModel();
+          expect(model.projectName).to.be.null;
         });
-    });
 
-    context ('#listByClient', () => {
-
-        it ('should be a method', () => {
-            expect(this.project.listByClient).to.be.a('function');
+        it ('should return undefined if parameter is a Number', function () {
+          let model = project.createModel();
+          model.projectName = 0;
+          expect(model.projectName).to.be.undefined;
+          model.projectName = 1;
+          expect(model.projectName).to.be.undefined;
+          model.projectName = 10;
+          expect(model.projectName).to.be.undefined;
         });
-    });
 
-    context ('#listByGroup', () => {
-
-        it ('should be a method', () => {
-            expect(this.project.listByGroup).to.be.a('function');
+        it ('should define the name if parameter is a string', function () {
+          let model = project.createModel();
+          model.projectName = '';
+          expect(model.projectName).to.equal('');
+          model.projectName = 'project';
+          expect(model.projectName).to.equal('project');
+          model.projectName = 'another project';
+          expect(model.projectName).to.equal('another project');
         });
-    });
 
-    context ('#listByName', () => {
-
-        it ('should be a method', () => {
-            expect(this.project.listByName).to.be.a('function');
+        it ('should redefine the name if already defined', function () {
+          let model = project.createModel();
+          model.projectName = 'project';
+          expect(model.projectName).to.equal('project');
+          model.projectName = '';
+          expect(model.projectName).to.equal('');
+          model.projectName = undefined;
+          expect(model.projectName).to.be.undefined;
         });
+
+        it ('should not redefine the name if already defined', function () {
+          let model = project.createModel();
+          model.projectName = 'project';
+          expect(model.projectName).to.equal('project');
+          model.projectName = 10;
+          expect(model.projectName).to.equal('project');
+        });
+
+        it ('should define the names of multiple projects', function () {
+          let model_1 = project.createModel();
+          model_1.projectName = 'project 1';
+          expect(model_1.projectName).to.equal('project 1');
+
+          let model_2 = project.createModel();
+          model_2.projectName = 'project 2';
+          expect(model_2.projectName).to.equal('project 2');
+        });
+
+      });
+
+      context ('.projectIcon', function () {
+        it ('should return null if no parameter passed', function () {
+          let model = project.createModel();
+          expect(model.projectIcon).to.be.null;
+        });
+
+        it ('should return undefined if parameter is a Number', function () {
+          let model = project.createModel();
+          model.projectIcon = 0;
+          expect(model.projectIcon).to.be.undefined;
+          model.projectIcon = 1;
+          expect(model.projectIcon).to.be.undefined;
+          model.projectIcon = 10;
+          expect(model.projectIcon).to.be.undefined;
+        });
+
+        it ('should set the name if parameter is a String', function () {
+          let model = project.createModel();
+          model.projectIcon = '';
+          expect(model.projectIcon).to.equal('');
+          model.projectIcon = 'icon';
+          expect(model.projectIcon).to.equal('icon');
+          model.projectIcon = 'another icon';
+          expect(model.projectIcon).to.equal('another icon');
+        });
+
+        it ('should redefine the name if already defined', function () {
+          let model = project.createModel();
+          model.projectIcon = 'icon';
+          expect(model.projectIcon).to.equal('icon');
+          model.projectIcon = '';
+          expect(model.projectIcon).to.equal('');
+          model.projectIcon = undefined;
+          expect(model.projectIcon).to.be.undefined;
+        });
+
+        it ('should not redefine the name if already defined', function () {
+          let model = project.createModel();
+          model.projectIcon = 'icon';
+          expect(model.projectIcon).to.equal('icon');
+          model.projectIcon = 10;
+          expect(model.projectIcon).to.equal('icon');
+        });
+
+      });
+
     });
+
+    context ('#createView', function () {
+
+      it ('is a public method', function () {
+        expect(project.createView).to.be.a('function');
+      });
+
+      it ('should return a DOM Node', function () {
+        let view = project.createView(project.createModel());
+        expect(view).to.be.an.instanceof(HTMLElement);
+      });
+
+    });
+
+  });
 
 });
--- src/project.js
+++ src/project.js
@@ -1,21 +1,98 @@
 'use strict';
 
-const project = Object.create(null);
+const projectMapper = new WeakMap();
 
-project.listAll = function _listAll () {
-    return [];
+function createModel () {
+  const project = Object.create(null, {
+    projectName: {
+      configurable: false,
+      enumerable: false,
+      set: function (name) {
+        let obj = projectMapper.get(this);
+        if (!obj) {
+          obj = {};
+          projectMapper.set(this, obj);
+        }
+        if (Number(name) === name) {
+          return;
+        }
+        obj.projectName = name;
+      },
+      get: function () {
+        let obj = projectMapper.get(this);
+        if (!obj) {
+          return null;
+        }
+        return obj.projectName;
+      }
+    },
+    projectIcon: {
+      configurable: false,
+      enumerable: false,
+      set: function (icon) {
+        let obj = projectMapper.get(this);
+        if (!obj) {
+          obj = {};
+          projectMapper.set(this, obj);
+        }
+        if (Number(icon) === icon) {
+          return;
+        }
+        obj.projectIcon = icon;
+      },
+      get: function () {
+        let obj = projectMapper.get(this);
+        if (!obj) {
+          return null;
+        }
+        return obj.projectIcon;
+      }
+    }
+  });
+  return Object.freeze(project);
+}
+
+const viewMethods = {
+  createdCallback: function createdCallback () {
+    let listItem = document.createElement('span');
+    listItem.classList.add('list-item');
+    this.appendChild(listItem);
+    console.debug(this);
+  }
 };
 
-project.listByClient = function _listByClient () {
-    return [];
-};
+function createView (model) {
+  const tagExtends = 'li';
+  const tagIs = 'project-viewer-project';
+  let viewConstructor;
+  let view;
 
-project.listByGroup = function _listByGroup () {
-    return [];
-};
+  try {
+    viewConstructor = document.registerElement(
+      tagIs,
+      {
+        prototype: viewMethods,
+        extends: tagExtends
+      }
+    );
+    Object.setPrototypeOf(viewMethods, HTMLElement.prototype);
+    view = new viewConstructor();
+  } catch (e) {
+    view =  document.createElement(tagIs, tagExtends);
+  }
+  projectMapper.set(view, model);
+  return view;
+}
 
-project.listByName = function _listByName () {
-    return [];
-};
+/**
+* Project.
+* @module project-viewer/project
+*/
 
-module.exports = project;
+module.exports = {
+  /** Create a project model */
+  createModel,
+
+  /** Create a project view given a project model */
+  createView,
+};
