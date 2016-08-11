b824350feafe653b64380dbefd58528f0accde1a
Roman Huba <roman.huba@gmail.com>
2016-08-11 23:29:12 +0200

    updateGist function refactored, added network exception handling
    Previously the function consisted of several nested if blocks which made it hard to follow what was going on, so I split it into smaller functions where each function does a specific task.
    Also previously uncought network exceptions are now handled and user is informed appropriately.

--- src/github-web-worker.js
+++ src/github-web-worker.js
@@ -1,47 +1,38 @@
 const api = {
+  url: 'https://api.github.com/gists',
+  gistDescription: 'atom.io project-viewer backup files',
   token: undefined,
   gistId: undefined,
   setName: 'default',
   get gistFileName() {
     return `project-viewer-${this.setName}.json`;
   },
-  url: 'https://api.github.com/gists',
-  gistDescription: 'atom.io project-viewer backup files',
-  checkToken: function checkToken() {
-    const promise = new Promise((resolve, reject) => {
-
-      // check if github access token is configured
-      if (!this.token) {
-        reject({
-          type: 'warning',
-          message: 'No <strong>Github Access Token</strong> was provided, please check the configuration.',
-          options: {
-            icon: 'mark-github'
-          }
-        });
-        return;
+  connectionError: function connectionError(error) {
+    return {
+      type: 'error',
+      message: `Failed to connect to <strong>GitHub</strong> servers: ${error.message}`,
+      options: {
+        icon: 'mark-github'
       }
-      resolve();
-    });
-
-    return promise;
+    };
   },
-  checkGistId: function checkGistId() {
+  // helper function to check whether given configuration value is defined
+  checkConfig: function checkConfig(value, name) {
     const promise = new Promise((resolve, reject) => {
 
-      // check if gist id is configured
-      if (!this.gistId) {
-        reject({
-          type: 'warning',
-          message: 'No <strong>Gist ID</strong> was provided, please check the configuration.',
-          options: {
-            icon: 'mark-github'
-          }
-        });
+      // check if given value is defined
+      if (value) {
+        resolve();
         return;
       }
 
-      resolve();
+      reject({
+        type: 'warning',
+        message: `No <strong>${name}</strong> was provided, please check the configuration.`,
+        options: {
+          icon: 'mark-github'
+        }
+      });
     });
 
     return promise;
@@ -60,32 +51,33 @@ const api = {
       };
 
       fetch(url, parameters)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          if (!data || !data.files || !data.files.hasOwnProperty(this.gistFileName)) {
-            // backup not found, either gist with given ID doesnt exist (user hasnt created the gist yet or it has been deleted) or user has no existing backup
-            reject({
-              type: 'warning',
-              message: `No backup found under gist ID [${this.gistId}] for set [${this.setName}]. Make sure that gist with given ID exists under your private gists and that you have an existing backup (call backup -> call import).`,
+        .then(this.toJson.bind(null, 200))
+        .then(data => {
+          if (data && data.files && data.files.hasOwnProperty(this.gistFileName)) {
+            // backup found, returning
+            resolve({
+              type: 'success',
+              message: 'Retrieved DB from <strong>GitHub</strong> successfully.',
+              db: JSON.parse(data.files[this.gistFileName].content),
               options: {
-                icon: 'mark-github',
-                dismissable: true
+                icon: 'mark-github'
               }
             });
             return;
           }
 
-          // backup found, returning
-          resolve({
-            type: 'success',
-            message: 'Retrieved DB from <strong>GitHub</strong> successfully.',
-            db: JSON.parse(data.files[this.gistFileName].content),
+          // backup not found, response status code was not 200 OK
+          // either gist with given ID doesnt exist (user hasnt created the gist yet or it has been deleted) or user has no existing backup
+          reject({
+            type: 'warning',
+            message: `No backup found under gist ID [${this.gistId}] for set [${this.setName}]. Make sure that gist with given ID exists under your private gists and that you have an existing backup (call backup -> call import).`,
             options: {
-              icon: 'mark-github'
+              icon: 'mark-github',
+              dismissable: true
             }
           });
+        }).catch(error => {
+          reject(this.connectionError(error));
         });
     });
 
@@ -93,131 +85,183 @@ const api = {
   },
   updateGist: function updateGist(value) {
     const promise = new Promise((resolve, reject) => {
+      let url = this.url + '/' + this.gistId;
 
-      // common headers
       let headers = new Headers();
       headers.append('Accept', 'application/vnd.github.v3+json');
       headers.append('Authorization', `token ${this.token}`);
 
-      if (this.gistId) {
-        // user provided gist id, check if gist exists (if yes update, otherwise reject)
-        let url = this.url + '/' + this.gistId;
+      let files = {};
+      files[this.gistFileName] = {
+        content: JSON.stringify(value)
+      };
 
-        let parameters = {
-          method: 'GET',
-          headers: headers,
-        };
+      let body = JSON.stringify({
+        description: this.gistDescription,
+        public: false,
+        files: files
+      });
 
-        fetch(url, parameters)
-          .then((response) => {
-            // if OK gist exists
-            if (response.ok) {
-              // update gist
-              let files = {};
-              files[this.gistFileName] = {
-                content: JSON.stringify(value)
-              };
+      parameters = {
+        method: 'PATCH',
+        headers: headers,
+        body: body
+      };
 
-              let body = JSON.stringify({
-                description: this.gistDescription,
-                public: false,
-                files: files
-              });
-
-              parameters = {
-                method: 'PATCH',
-                headers: headers,
-                body: body
-              };
-
-              fetch(url, parameters)
-                .then((response) => {
-                  if (response.ok) {
-                    // gist successfully updated
-                    resolve({
-                      type: 'success',
-                      message: 'Successfully backed up the DB.',
-                      options: {
-                        icon: 'mark-github'
-                      }
-                    });
-                    return;
-                  }
-
-                  reject({
-                    type: 'warning',
-                    message: 'Failed to update gist.',
-                    options: {
-                      icon: 'mark-github'
-                    }
-                  });
-                  return;
-                });
-            } else {
-              // non-OK response from GET on /gists/{gistId} -> gist with user provided gist id doesnt exist
-              reject({
-                type: 'warning',
-                message: `No gist found with ID [${this.gistId}]. Specify valid gist ID or specify empty gist ID and we will create a gist for you.`,
-                options: {
-                  icon: 'mark-github',
-                  dismissable: true
-                }
-              });
-              return;
-            }
-          });
-      } else {
-        // user didnt specify gist id, create gist for him and set it in config
-        let files = {};
-        files[this.gistFileName] = {
-          content: JSON.stringify(value)
-        };
-
-        let body = JSON.stringify({
-          description: this.gistDescription,
-          public: false,
-          files: files
-        });
-
-        let parameters = {
-          method: 'POST',
-          headers: headers,
-          body: body
-        };
-
-        fetch(this.url, parameters)
-          .then((response) => {
-            return response.json();
-          })
-          .then((data) => {
-            if (data && data.id) {
-              // gist successfully created
-              this.gistId = data.id;
-
-              resolve({
-                type: 'success',
-                message: `Successfully created gist ID [${data.id}] and backed up the DB.`,
-                options: {
-                  icon: 'mark-github'
-                },
-                gistId: data.id
-              });
-              return;
-            }
-
-            reject({
-              type: 'warning',
-              message: 'Failed to create gist.',
+      fetch(url, parameters)
+        .then((response) => {
+          if (response.status == 200) {
+            // gist successfully updated
+            resolve({
+              type: 'success',
+              message: 'Successfully backed up the DB.',
               options: {
                 icon: 'mark-github'
               }
             });
             return;
+          }
+
+          // failed to update gist, reponse status code was not 200 OK
+          reject({
+            type: 'warning',
+            message: 'Failed to update gist.',
+            options: {
+              icon: 'mark-github'
+            }
           });
+        }).catch(error => {
+          reject(this.connectionError(error));
+        });
+    });
+
+    return promise;
+  },
+  checkIfGistExists: function checkIfGistExists() {
+    const promise = new Promise((resolve, reject) => {
+      let url = this.url + '/' + this.gistId;
+
+      let headers = new Headers();
+      headers.append('Accept', 'application/vnd.github.v3+json');
+      headers.append('Authorization', `token ${this.token}`);
+
+      let parameters = {
+        method: 'GET',
+        headers: headers,
+      };
+
+      fetch(url, parameters)
+        .then(response => {
+          if (response.status == 200) {
+            // gist exists
+            resolve();
+            return;
+          }
+
+          // gist doesn't exist
+          reject({
+            type: 'warning',
+            message: `No gist found with ID [${this.gistId}]. Specify valid gist ID or specify empty gist ID and we will create a gist for you.`,
+            options: {
+              icon: 'mark-github',
+              dismissable: true
+            }
+          });
+        }).catch(error => {
+          reject(this.connectionError(error));
+        });
+    });
+
+    return promise;
+  },
+  createNewGist: function createNewGist(value) {
+    const promise = new Promise((resolve, reject) => {
+      let headers = new Headers();
+      headers.append('Accept', 'application/vnd.github.v3+json');
+      headers.append('Authorization', `token ${this.token}`);
+
+      let files = {};
+      files[this.gistFileName] = {
+        content: JSON.stringify(value)
+      };
+
+      let body = JSON.stringify({
+        description: this.gistDescription,
+        public: false,
+        files: files
+      });
+
+      let parameters = {
+        method: 'POST',
+        headers: headers,
+        body: body
+      };
+
+      fetch(this.url, parameters)
+        .then(this.toJson.bind(null, 201))
+        .then(data => {
+          if (data && data.id) {
+            // gist successfully created
+            this.gistId = data.id;
+
+            resolve({
+              type: 'success',
+              message: `Successfully created gist ID [${data.id}] and backed up the DB.`,
+              options: {
+                icon: 'mark-github'
+              },
+              gistId: data.id
+            });
+            return;
+          }
+
+          // failed to craete gist, response status code was not 201 Created
+          reject({
+            type: 'warning',
+            message: 'Failed to create gist.',
+            options: {
+              icon: 'mark-github'
+            }
+          });
+        }).catch(error => {
+          reject(this.connectionError(error));
+        });
+    });
+
+    return promise;
+  },
+  // orchestration function for update operation
+  updateOperation: function updateOperation(value) {
+    const promise = new Promise((resolve, reject) => {
+      if (this.gistId) {
+        // user provided gist id, check if gist exists (if yes update, otherwise reject)
+        this.checkIfGistExists()
+          .then(() => {
+            this.updateGist(value).then(resolve).catch(reject);
+          }).catch(reject);
+      } else {
+        // user didnt specify gist id, create gist for him and set it in config
+        this.createNewGist(value).then(resolve).catch(reject);
       }
     });
 
     return promise;
+  },
+  // orchestration function for fetch operation
+  fetchOperation: function fetchOperation() {
+    const promise = new Promise((resolve, reject) => {
+      this.getGist().then(resolve).catch(reject);
+    });
+
+    return promise;
+  },
+  // helper function to check whether the response status code equals the requiredStatusCode and return json promise if so
+  // we then check in Promise.then() if data exists (exists only if response respones status code matched)
+  toJson: function toJson(requiredStatusCode, response) {
+    if (response.status == requiredStatusCode) {
+      return response.json();
+    }
+    return Promise.resolve(undefined);
   }
 };
 
@@ -239,17 +283,17 @@ onmessage = function(e) {
   }
 
   if (e.data[0].action === 'fetch') {
-    Promise.all([api.checkToken(), api.checkGistId()])
+    Promise.all([api.checkConfig(api.token, 'Github Access Token'), api.checkConfig(api.gistId, 'Gist ID')])
       .then(() => {
-        api.getGist()
+        api.fetchOperation()
           .then(postMessage)
           .catch(postMessage);
       })
       .catch(postMessage);
   } else if (e.data[0].action === 'update') {
-    Promise.all([api.checkToken()])
+    Promise.all([api.checkConfig(api.token, 'Github Access Token')])
       .then(() => {
-        api.updateGist(e.data[0].value)
+        api.updateOperation(e.data[0].value)
           .then(postMessage)
           .catch(postMessage);
       })
