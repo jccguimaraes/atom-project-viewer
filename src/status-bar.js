d4cdaf95efb8a4a8b8515c2689c04391722dfd8b
João Guimarães <jccguimaraes@gmail.com>
2017-05-22 12:02:18 +0100

    :bug: try to fix #149

--- src/status-bar.js
+++ src/status-bar.js
@@ -2,7 +2,11 @@ const map = require('./map');
 
 const update = function _update (text) {
   if (!text) { return; }
+  if (!statusBar.view && typeof statusBar.view.getItem !== 'function') {
+    return;
+  }
   const item = statusBar.view.getItem();
+  if (!item) { return; }
   item.textContent = text;
 };
 
